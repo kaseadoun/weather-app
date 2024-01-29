a1851c9cbf66ecdd4648b01bf9a32f1f908c8e1d
Karsten Leung <karstenkleung@gmail.com>
2024-01-28 23:01:59 -0500

    Uploade dist folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://kaseadoun.github.io/weather-app/"
+  base:"/weather-app/"
 })
