4837de1dc6b69a09275f80b4671a5ec08b9fbad5
Karsten Leung <karstenkleung@gmail.com>
2024-01-29 13:00:22 -0500

    Fixed config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://kaseadoun.github.io/weather-app/",
+  base:"/weather-app/",
   plugins: [react()]
 })
