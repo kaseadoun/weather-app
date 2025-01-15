7d18fd9b1f27e07da6320e850c28cac1e7295581
Karsten <karstenkleung@gmail.com>
2025-01-15 14:49:25 -0500

    Changed layout of headers

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -4,7 +4,8 @@ import Forecast from "./Forecast";
 const WeatherCard = ({ location, current, currentCondition, forecast, isCelsius }) => {
   return (
     <>
-      <h2>{location["name"]}, {location["region"] ? `${location["region"]} ${location["country"]}` : location["country"]}</h2>
+      <h2>{location["name"]},</h2>
+      <h3>{location["region"] ? `${location["region"]}, ${location["country"]}` : location["country"]}</h3>
       <div className="currentAndForecast">
         <CurrentWeather
           current={current}
