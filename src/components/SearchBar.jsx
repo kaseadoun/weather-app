9bed7307f770f04f66e3a3174bb5ae9acea1a949
Karsten <karstenkleung@gmail.com>
2025-01-15 13:18:31 -0500

    Completed search filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SearchBar = ({ search, setSearch, searchCityById }) => {
+  const [isFocused, setIsFocused] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [listOfSearch, setListOfSearch] = useState([]);
+
+  useEffect(() => {
+    if (searchCityById) {
+      searchCityById(search); // Runs whenever `search` updates
+    }
+  }, [searchCityById]);
+
+  function handleOnFocus() {
+    setIsFocused(true);
+  }
+
+  function handleOnBlur() {
+    setIsFocused(false);
+  }
 
   const api_key = import.meta.env.VITE_WEATHERAPI_API_KEY;
   const base_url = "https://api.weatherapi.com/v1/";
@@ -33,36 +48,50 @@ const SearchBar = ({ search, setSearch, searchCityById }) => {
     <li
       key={item.id}
       onClick={() => {
-        setSearch(`id:${item.id}`);
-        searchCityById(search)
+        const selectedCityId = `id:${item.id}`;
+        setSearch(selectedCityId);
+        searchCityById(selectedCityId);
+        setIsFocused(() => handleOnBlur());
       }}
+      className="autocompleteSearchResults"
     >
       {item.city}, {item.region}, {item.country}
     </li>
   ));
 
   return (
-    <>
+    <div className="searchBarContainer">
       <form className="searchBarComponent" onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           placeholder={`🔍 Search City`}
           id="search"
           onChange={(e) => {
-            setSearch(e.target.value);
+            setSearchText(e.target.value);
             searching(e.target.value);
           }}
+          onFocus={handleOnFocus}
           autoComplete="off"
           required
         />
         <button
           id="searchButton"
           type="submit"
-          onClick={() => searchCityById(search)}
+          onClick={() => {
+            setSearch(searchText);
+            searchCityById(search);
+          }}
         ></button>
       </form>
-      <ul>{listOfCities}</ul>
-    </>
+      <ul
+        className="autocompleteSearchResultsContainer"
+        style={{
+          display: isFocused ? "block" : "none",
+        }}
+      >
+        {listOfCities}
+      </ul>
+    </div>
   );
 };
 
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -4,7 +4,7 @@ import Forecast from "./Forecast";
 const WeatherCard = ({ location, current, currentCondition, forecast, isCelsius }) => {
   return (
     <>
-      <h2>{location["name"]}, {location["region"] ? location["region"] : location["country"]}</h2>
+      <h2>{location["name"]}, {location["region"] ? `${location["region"]} ${location["country"]}` : location["country"]}</h2>
       <div className="currentAndForecast">
         <CurrentWeather
           current={current}
