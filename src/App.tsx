6b30ba0205a630428594076a35812f4d62c0e6a9
Karsten <karstenkleung@gmail.com>
2025-01-18 13:14:15 -0500

    Removed async func

--- src/App.tsx
+++ src/App.tsx
@@ -13,40 +13,6 @@ function App() {
   const [forecast, setForecast] = useState<ForecastDay[]>([]);
   const [chartData, setChartData] = useState<Object>({});
 
-  // const searchCityById = async (cityId: string) => {
-  //   try {
-  //     const response = await fetch(`${url}&q=${search}&days=3&aqi=no&alerts=no`);
-  //     const data = await response.json();
-  //     console.log(data);
-  //     setForecast(data["forecast"]["forecastday"]);
-  //     setLocation({ ...data["location"] });
-  //     setCurrent({ ...data["current"] });
-  //     setCurrentCondition({ ...data["current"]["condition"] });
-
-  //     setChartData(() => {
-  //       const labels: number[] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-  //       const label = `${data["location"]}`;
-  //       const hourlyPrecipitation: number[] = data["forecast"]["forecastday"][0]["hour"].map((data) => data["chance_of_rain"]);
-
-  //       return {
-  //         labels: labels,
-  //         datasets: [
-  //           {
-  //             label: label,
-  //             data: hourlyPrecipitation
-  //           }
-  //         ]
-  //       }
-  //     })
-
-  //     setLoading(true);
-  //   } catch (err) {
-  //     console.error(`Error: ${err}`);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-
   return (
     <div className="mainContainer">
       <header>
